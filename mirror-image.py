76bb3c36ca9649e916574e81c74ca0e517b95506
solsahn <solsahn@gmail.com>
2018-08-31 13:38:21 -0400

    script checks if file is an image, throws a custom error if not

--- mirror-image.py
+++ mirror-image.py
@@ -11,22 +11,30 @@ saveImg = True
 
 # Takes user input for file path and checks if the file existsfilePath = input("enter path to image: ")
 if(os.path.isfile(filePath)):
-    img = Image.open(filePath)
-    # Copies image, flips it, crops out the unneeded side and pastes back onto image
-    mirror = img.copy()
-    mirror = mirror.transpose(Image.FLIP_LEFT_RIGHT)
-    # Allows user to select which side of image to be mirrored
-    side = input("which side of the picture should be mirrored? (r/l) ")
-    if(side == "l"):
-        mirror = mirror.crop((img.size[0]/2, 0, img.size[0], img.size[1]))
-        img.paste(mirror, ((int(img.size[0]/2), 0)))
-    elif (side == "r"):
-        mirror = mirror.crop((0, 0, img.size[0]/2, img.size[1]))
-        img.paste(mirror, (0, 0))
-    else:
-        # Cancels if image side is invalid
-        print("please enter a valid side")
+
+    # Checks if file is an image, continues if successful, throws custom error if not and cancels image save
+    try:
+        Image.open(filePath)
+    except IOError:
+        print("file is not an image")
         saveImg = False
+    else:
+        img = Image.open(filePath)
+        # Copies image, flips it, crops out the unneeded side and pastes back onto image
+        mirror = img.copy()
+        mirror = mirror.transpose(Image.FLIP_LEFT_RIGHT)
+        # Allows user to select which side of image to be mirrored
+        side = input("which side of the picture should be mirrored? (r/l) ")
+        if(side == "l"):
+            mirror = mirror.crop((img.size[0]/2, 0, img.size[0], img.size[1]))
+            img.paste(mirror, ((int(img.size[0]/2), 0)))
+        elif (side == "r"):
+            mirror = mirror.crop((0, 0, img.size[0]/2, img.size[1]))
+            img.paste(mirror, (0, 0))
+        else:
+            # Cancels if image side is invalid
+            print("please enter a valid side")
+            saveImg = False
         
 else:
     # Cancels if image does not exist
